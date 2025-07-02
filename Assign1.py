4f90781b340ffbd35f5dbbcebca608500d79a9ad
renisweta100 <78549815+renisweta100@users.noreply.github.com>
2025-07-02 18:51:18 +0530

    Add files via upload

---
+++ Assign1.py
@@ -0,0 +1,16 @@
+
+#Print Operation
+a=int(input("Enter the First number"))
+b=int(input("Enter the second number"))
+print("Addition : ",a+b)
+print("Subtraction : ",a-b)
+print("Multiplication : ",a*b)
+print("Divison : ",a/b)
+
+
+
+
+#Print Hello
+first = input("Enter your first name: ")
+sec = input("Enter your last name :")
+print("Hello, {} {}! Welcome to the Python program.".format(first,sec))
