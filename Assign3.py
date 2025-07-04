06783b3db3a385a3951badbf5f7cc0145ec6dc86
renisweta100 <78549815+renisweta100@users.noreply.github.com>
2025-07-04 14:01:51 +0530

    Add files via upload

---
+++ Assign3.py
@@ -0,0 +1,25 @@
+
+#Student's Marks
+student_data = {"toddy":24,"ami":67,"yana":90}
+ask = input("Enter student's name: ")
+if ask in student_data.keys():
+    print("{} mark's :{}".format(ask,student_data[ask]))
+else:
+    print("Student not found.")
+
+factorial(num)
+
+
+
+#reverse list
+l = [1,2,3,4,5,6,7,8,9,10]
+size = int(len(l)/2)
+new_list=[]
+
+for i in range(size):
+    new_list.append(l[i])
+
+
+print("Original list: ",l)
+print("Extracted first five items: ",new_list)
+print("Reversed extracted elements: ",new_list[::-1])
