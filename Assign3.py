f1305fe735988b3aa8a73241efc0eefec16bbe1d
renisweta100 <78549815+renisweta100@users.noreply.github.com>
2025-07-05 15:59:24 +0530

    Update Assign3.py

--- Assign3.py
+++ Assign3.py
@@ -1,25 +1,21 @@
+#Factorial
+num = int(input("Enter a number"))
+fact = 0
+def factorial(num):
+    if num == 0:
+        return 1
 
-#Student's Marks
-student_data = {"toddy":24,"ami":67,"yana":90}
-ask = input("Enter student's name: ")
-if ask in student_data.keys():
-    print("{} mark's :{}".format(ask,student_data[ask]))
-else:
-    print("Student not found.")
+    else:
+        fact = num*(num-1)
+        print("Factorial of {} is: {}".format(num,fact))
 
 factorial(num)
 
 
 
-#reverse list
-l = [1,2,3,4,5,6,7,8,9,10]
-size = int(len(l)/2)
-new_list=[]
-
-for i in range(size):
-    new_list.append(l[i])
-
-
-print("Original list: ",l)
-print("Extracted first five items: ",new_list)
-print("Reversed extracted elements: ",new_list[::-1])
+#Math operations
+num=int(input("Enter a number"))
+import math
+print("Square root: ",math.sqrt(num))
+print("Logarithm :",math.log(num))
+print("Sine: ",math.sin(num))
